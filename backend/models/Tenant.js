76e3b6e20e4aaea56903d5a775065d2a6229132e
nani-tyson <nanityson10@gmail.com>
2025-09-13 21:01:38 +0000

    temporarily backend removed

--- backend/models/Tenant.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const TenantSchema = new Schema({
-  name: { type: String, required: true },
-  slug: { type: String, required: true, unique: true },
-  plan: { type: String, enum: ['free', 'pro'], default: 'free' },
-});
-
-export default model('Tenant', TenantSchema);
